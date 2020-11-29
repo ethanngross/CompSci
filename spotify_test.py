bbb1252e48a82da05b61aafdbe5e7832f631b8e3
ethan <ethancgross18@gmail.com>
2020-11-28 20:06:32 -0500

    nada

--- spotify_test.py
+++ spotify_test.py
@@ -2,8 +2,8 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from spotipy.oauth2 import SpotifyOAuth
 
-set SPOTIPY_CLIENT_ID= e53291ba5a474c7e852304474c92e1d8
-set SPOTIPY_CLIENT_SECRET= 6a6bc9c277d94480b46f47c37fec0493
+set SPOTIPY_CLIENT_ID= 
+set SPOTIPY_CLIENT_SECRET= 
 set SPOTIPY_REDIRECT_URI= http://example.com
 
 birdy_uri = 'spotify:artist:2WX2uTcsvV5OnS0inACecP'
